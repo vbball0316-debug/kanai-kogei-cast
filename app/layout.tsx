ab422a1d42af48f53053915cb2603e62af85fba6
TK <tk>
2026-08-13 14:53:51 +0900

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,8 @@ export const metadata = {
   title: "株式会社 金井工芸鋳造所｜仏具・美術工芸品の鋳造",
   description:
     "株式会社金井工芸鋳造所は、仏具をはじめ、美術工芸品の鋳物など、さまざまな鋳造品を製造しています。培ってきた鋳造技術と経験を活かし、確かなものづくりを続けています。",
+  verification: {
+    google: "aKbTLrGwRuohca_OYTDeF2s7er0F-IC-txjHMLnGliI",
 };
 
 
